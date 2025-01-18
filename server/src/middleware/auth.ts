fc6ef571bd6d189d2f452bbe013f0bc0f4b519f7
Matt Oberdalhoff <reliable.vagabond@gmail.com>
2025-01-18 13:26:31 -0600

    added code needed for authAPI, auth-routes for JWT autorization

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -6,5 +6,22 @@ interface JwtPayload {
 }
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-  // TODO: verify the token exists and add the user data to the request object
+  const authHeader = req.headers.authorization; //check to see if request even has a header to start with
+  
+  if (authHeader) { //if it does we can start checking JWT authorization
+    const token = authHeader.split(' ')[1];
+
+    const secretKey = process.env.JWT_SECRET_KEY || '';
+
+    jwt.verify(token, secretKey, (err, user) => {
+      if (err) {
+        return res.sendStatus(403); // Forbidden. this is because the token didn't authorize, because it wasnt the right key
+      }
+
+      req.user = user as JwtPayload;
+      return next();
+    });
+  } else {
+    res.sendStatus(401); // Unauthorized, as it didn't even have a authorization header to start with
+  }
 };
--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -4,7 +4,25 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
-  // TODO: If the user exists and the password is correct, return a JWT token
+  
+  const { username, password } = req.body; //get the username and password from the login request
+
+  const user = await User.findOne({ //search for the user
+    where: { username },
+  });
+  if (!user) { //if no user is found, you cant login!
+    return res.status(401).json({ message: 'Authentication failed' });
+  }
+
+  const passwordIsValid = await bcrypt.compare(password, user.password); //compare the password given with the encryted one
+  if (!passwordIsValid) { //if they don't match, auth fails
+    return res.status(401).json({ message: 'Authentication failed' });
+  }
+
+  const secretKey = process.env.JWT_SECRET_KEY || '';
+
+  const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' }); //if they do, sign a JWT token and pass it on
+  return res.json({ token });
 };
 
 const router = Router();
